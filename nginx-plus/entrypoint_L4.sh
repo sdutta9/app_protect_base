085eb09371936435fbd5b7915618587c1c70eef0
Shouvik <shouvik.dutta96@gmail.com>
2023-03-01 15:38:07 -0600

    updates to install default dos without L4 accelarated mitigation

--- nginx-plus/entrypoint_L4.sh
+++ nginx-plus/entrypoint_L4.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-   
-USER=nginx
+  
 LOGDIR=/var/log/adm
    
 # prepare environment
@@ -9,6 +8,6 @@ chmod 755 /var/run/adm /tmp/cores ${LOGDIR}
 chown ${USER}:${USER} /var/run/adm /tmp/cores ${LOGDIR}
    
 # run processes
-/bin/su -s /bin/bash -c "/usr/bin/adminstall --daemons 1 --memory 200 > ${LOGDIR}/adminstall.log 2>&1" ${USER}
+/bin/su -s /bin/bash -c "ulimit -l unlimited && /usr/bin/adminstall -e > ${LOGDIR}/admd.log 2>&1" ${USER}
 /usr/sbin/nginx -g 'daemon off;' &
 /bin/su -s /bin/bash -c "/usr/bin/admd -d --log info > ${LOGDIR}/admd.log 2>&1 &" ${USER}
